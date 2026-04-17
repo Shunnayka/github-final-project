3e8226dafe0e0af06420a688cec0c66b5774fc41
Shunnayka <pelusaprojiji@gmail.com>
2026-04-17 18:13:42 -0500

    Add simple-interest.sh script for calculating interest

---
+++ simple-interest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Ingresa el capital (principal):"
+read p
+echo "Ingresa la tasa de interés anual:"
+read r
+echo "Ingresa el período de tiempo en años:"
+read t
+
+s=`expr $p \* $t \* $r / 100`
+echo "El interés simple es: $s"
